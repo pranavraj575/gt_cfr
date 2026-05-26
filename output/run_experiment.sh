e5ac88d82038d81846cf87adf9a450057b04acb5
pranav <pranavraj575@gmail.com>
2026-05-25 20:02:20 -0400

    does this work better

--- output/run_experiment.sh
+++ output/run_experiment.sh
@@ -4,7 +4,5 @@
 #SBATCH --time=180:00:00
 #SBATCH --mem=48G
 #SBATCH --exclude=marvel-0-29,marvel-1-[19,21,23,25,27]
-#SBATCH --output=./my_script_$1.out
-#SBATCH --error=./my_script_$1.err
 
 python xdo.py $1
